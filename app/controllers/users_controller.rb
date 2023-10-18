48b1873dfd589000517fcd0aa449ab17e006c1d9
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-18 11:13:52 +0000

    showまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
@@ -17,5 +24,8 @@ class UsersController < ApplicationController
    def book_params
     params.require(:book).permit(:title, :body)
    end
-
+   
+   def user_params
+     params.require(:user).permit(:name, :profile_image, :introduction)
+   end
 end
