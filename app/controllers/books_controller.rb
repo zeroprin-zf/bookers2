333e32853dc4a224ea7bbbb79305decc0994d36e
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-21 08:10:48 +0000

    中身はできた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     @books = Book.all
     #@book_a = Book.find(params[:id])
     @user = current_user
+    
   end
 
   def create
@@ -18,19 +19,25 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)#book_path(@book.id)
     else
-      render :books_path
+      @books = Book.all
+      render :index
     end
   end
 
   def edit
+    book = Book.find(params[:id])
+    unless book.user == current_user #エラー出るかも
+      redirect_to user_path(current_user)
+    end
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
      flash[:notice] = "You have updated user successfully."
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)#なんでこうなる？
     else
      render :edit
     end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+  validates :title, presence: true
+  validates :body, length: {in: 1..200}
+
   #def get_image
     #unless image.attached?
      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
