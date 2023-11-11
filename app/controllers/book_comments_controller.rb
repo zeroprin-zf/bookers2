ad455b5614688b2c717be0eeaba161cdc1a69486
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-11-11 07:49:12 +0000

    いいねとコメント非同期できた

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
